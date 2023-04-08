d3fab23aa28a0152dd07a1d0cd7e990d35d2a32e
Priyankaa2503 <priyankaar25@gmail.com>
2023-04-09 05:26:57 +0530

    added color palatte

---
+++ lib/utils/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+Color primary = const Color(0xFF687daf);
+class Styles{
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0xFF454545);
+  static Color bgColor = const Color(0xFFFFE6C7);
+  static Color lightOrangeColor = const Color(0xFFFF6000);
+  static Color orangeColor = const Color(0xFFFF6000);
+  static TextStyle textStyle = TextStyle(fontSize: 16, color: textColor,fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle1 = TextStyle(fontSize: 26, color: textColor,fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle2 = TextStyle(fontSize: 21, color: textColor,fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle3 = const TextStyle(fontSize: 17, fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle4 = TextStyle(fontSize: 14, color: Colors.grey.shade500 ,fontWeight: FontWeight.w500);
+}
